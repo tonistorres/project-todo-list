c791625db1a04626edacf6540a98100506df8a25
tonistorres <sisvenda2011@gmail.com>
2021-09-13 18:28:41 -0300

    QT 10 Respondida e Avalida Linter

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//******************************************* */
+//Fonte de Pesquisa:
+//https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+//https://www.ti-enxame.com/pt/javascript/como-posso-remover-um-estilo-adicionado-com-funcao-.css/970783861/
+//https://www.w3schools.com/
+//********************************************* */
+//Implementação: Tonis Torres
+//********************************************* */
+
+
 /*após o carregamento dos elementos HTML na página 
 execute as funções dentro de window.onload*/
 window.onload = function () {
@@ -10,6 +20,8 @@ ao Button no documento HTML. A mesma é capturada pelo método
 getElementById por meio do id da tag buttom 
 */
 const buttonCreateListAssignment = document.getElementById('criar-tarefa');
+const buttonClearList = document.getElementById('apaga-tudo');
+
 let eventTarget;
 
 /*a variável capitura a Tag HTML onde se encontra o texto que será 
@@ -26,34 +38,32 @@ function addAssignment() {
 }
 
 /** INICIO CRIA ELEMENTO LI E CAPTURA O VALOR DIGITADO ADICIONA LISTA **/
-function criateElementLI(ev) {
+function criateElementLI() {
     let containerList = document.querySelector('.tarefas');
     let listItem = document.createElement('li');
     listItem.setAttribute("class", "lista-itens")
-    listItem.addEventListener("click",functionColor);
+    listItem.addEventListener("click", functionColor);
     listItem.innerText += document.querySelector('#texto-tarefa').value;
     containerList.appendChild(listItem);
 }
 
 /********INICIO FUNÇÃO BACKGROUND******/
-function functionColor(event){
-
-eventTarget=document.querySelectorAll('.lista-itens');
-
-alert('escuta colocada:'+eventTarget)
-
-for (let i = 0; i < eventTarget.length; i++) {
-    eventTarget[i].style.backgroundColor="transparent";
-    
-    
+function functionColor(event) {
+    eventTarget = document.querySelectorAll('.lista-itens');
+    for (let i = 0; i < eventTarget.length; i+=1) {
+        eventTarget[i].style.backgroundColor = "transparent";
+    }
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
 }
 
-//event.target.style.backgroundColor="transparent";
-event.target.style.backgroundColor="rgb(128, 128, 128)";
-
+/*******INICIO FUNÇÃO APAGA TUDO***************  */
+function clearAllToDoList() {
+    let containerListAll = document.querySelectorAll('.tarefas');
+    for (let i = 0; i < containerListAll.length; i+=1) {
+        containerListAll[i].innerHTML="";
+    }
 }
 
+/*********INICIO FUNÇÃO APAGA TUDO **************/
 
-
-
-
+buttonClearList.addEventListener("click",clearAllToDoList)
