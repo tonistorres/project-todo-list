b14a4c536e5f532fee1b8e435e6e20bfb306b379
tonistorres <sisvenda2011@gmail.com>
2021-09-13 17:24:49 -0300

    QT 07 Respondida e Avaliada npm test

--- script.js
+++ script.js
@@ -13,30 +13,46 @@ const buttonCreateListAssignment = document.getElementById('criar-tarefa');
 
 /*a variável capitura a Tag HTML onde se encontra o texto que será 
 digitado pelo usuario para ser adicionada na lista de tarefas*/
-let caputeTextAssignment=document.getElementById('texto-tarefa');
+let caputeTextAssignment = document.getElementById('texto-tarefa');
 
 
 /********* INICIO ESCUTA E FUNÇÃO ADICIONAR TAREFAS A LISTA ****** */
 buttonCreateListAssignment.addEventListener("click", addAssignment);
+
 function addAssignment() {
-    
-criateElementLI();
-caputeTextAssignment.value="";
+    criateElementLI();
+    //colar escuta na hora da criação 
+
+    caputeTextAssignment.value = "";
 }
-/********* FIM ESCUTA E FUNÇÃO ADICIONAR TAREFAS A LISTA ****** */
 
 
 /** INICIO CRIA ELEMENTO LI E CAPTURA O VALOR DIGITADO ADICIONA LISTA **/
-function criateElementLI(ev){
-    let containerList= document.querySelector('.tarefas');
+function criateElementLI(ev) {
+    let containerList = document.querySelector('.tarefas');
     let listItem = document.createElement('li');
     /*listItem que é o elemento que está sendo criado. Recebe 
     a query hora setada no input que recebe o valor digitado 
     pelo usuário o value esse valor digitado é adicionado na 
     li criado e colocado na lista 
     */
-    listItem.innerText+=document.querySelector('#texto-tarefa').value;
-    console.log(listItem.innerText)
+    listItem.setAttribute("class", "lista-itens")
+    
+    //colocar escuta de Click nos elementos criados
+    listItem.addEventListener("click",functionColor);
+    //Recuperando texto digitado pelo usuario
+    listItem.innerText += document.querySelector('#texto-tarefa').value;
+    //adicionado o elemento criado na lista Ordenada
     containerList.appendChild(listItem);
 }
-/** FIM CRIA ELEMENTO LI E CAPTURA O VALOR DIGITADO ADICIONA LISTA **/
+
+/********INICIO FUNÇÃO BACKGROUND******/
+function functionColor(event){
+alert('escuta colocada')
+document.querySelector('.lista-itens').style.background="rgb(128, 128, 128)";
+}
+
+
+
+
+
