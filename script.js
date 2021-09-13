5d34e8427f37bf212ad7d323dafa5f7c8d381cd5
tonistorres <sisvenda2011@gmail.com>
2021-09-13 18:09:37 -0300

    QT 08 Respondida e Avaliada npm test

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ ao Button no documento HTML. A mesma é capturada pelo método
 getElementById por meio do id da tag buttom 
 */
 const buttonCreateListAssignment = document.getElementById('criar-tarefa');
+let eventTarget;
 
 /*a variável capitura a Tag HTML onde se encontra o texto que será 
 digitado pelo usuario para ser adicionada na lista de tarefas*/
@@ -21,35 +22,35 @@ buttonCreateListAssignment.addEventListener("click", addAssignment);
 
 function addAssignment() {
     criateElementLI();
-    //colar escuta na hora da criação 
-
     caputeTextAssignment.value = "";
 }
-
 
 /** INICIO CRIA ELEMENTO LI E CAPTURA O VALOR DIGITADO ADICIONA LISTA **/
 function criateElementLI(ev) {
     let containerList = document.querySelector('.tarefas');
     let listItem = document.createElement('li');
-    /*listItem que é o elemento que está sendo criado. Recebe 
-    a query hora setada no input que recebe o valor digitado 
-    pelo usuário o value esse valor digitado é adicionado na 
-    li criado e colocado na lista 
-    */
     listItem.setAttribute("class", "lista-itens")
-    
-    //colocar escuta de Click nos elementos criados
     listItem.addEventListener("click",functionColor);
-    //Recuperando texto digitado pelo usuario
     listItem.innerText += document.querySelector('#texto-tarefa').value;
-    //adicionado o elemento criado na lista Ordenada
     containerList.appendChild(listItem);
 }
 
 /********INICIO FUNÇÃO BACKGROUND******/
 function functionColor(event){
-alert('escuta colocada')
-document.querySelector('.lista-itens').style.background="rgb(128, 128, 128)";
+
+eventTarget=document.querySelectorAll('.lista-itens');
+
+alert('escuta colocada:'+eventTarget)
+
+for (let i = 0; i < eventTarget.length; i++) {
+    eventTarget[i].style.backgroundColor="transparent";
+    
+    
+}
+
+//event.target.style.backgroundColor="transparent";
+event.target.style.backgroundColor="rgb(128, 128, 128)";
+
 }
 
 
